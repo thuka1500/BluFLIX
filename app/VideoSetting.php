59e9d17c6a4b2b9946621265978ac6ef73f627e0
HDVinnie <unit3d@protonmail.com>
2018-09-15 22:48:40 -0400

    (Add) Video Models

---
+++ app/VideoSetting.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VideoSetting extends Model
+{
+    /**
+     * Belongs To A User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault([
+            'username' => 'System',
+            'id' => '1'
+        ]);
+    }
+}
