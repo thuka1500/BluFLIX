cb2276cbe49fbc1be5cb1a8fbb76e5f3d884089d
HDVinnie <unit3d@protonmail.com>
2018-09-15 22:48:50 -0400

    (Update) User Model

--- app/User.php
+++ app/User.php
@@ -10,20 +10,12 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
+     * The Attributes Excluded From The Model's JSON Form.
      *
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token'
     ];
 }
