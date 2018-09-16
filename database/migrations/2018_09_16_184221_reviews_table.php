2566ef037d2a3abaf494a79b14447610b3b6ac4b
HDVinnie <unit3d@protonmail.com>
2018-09-16 14:51:04 -0400

    (Add) Review Migration

---
+++ database/migrations/2018_09_16_184221_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discussions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('video_id')->unsigned()->index();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->longText('content');
+            $table->decimal('score', 9, 2)->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('episode_id')->references('id')->on('episodes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
