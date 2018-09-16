006512c70feb55959a4e5cd79df0ae0803d2c236
HDVinnie <unit3d@protonmail.com>
2018-09-15 23:12:27 -0400

    (Add) Genre Model

---
+++ app/Genre.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Genre extends Model
+{
+    /**
+     * Indicates If The Model Should Be Timestamped
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
