b8a8ff783e6874617ea4a58ba1a9942c7d8e146a
HDVinnie <unit3d@protonmail.com>
2018-09-15 23:13:02 -0400

    (Add) Network Model

---
+++ app/Network.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Network extends Model
+{
+    /**
+     * Indicates If The Model Should Be Timestamped
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
