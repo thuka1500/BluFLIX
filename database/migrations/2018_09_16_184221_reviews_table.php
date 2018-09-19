5aed576170f5d4e1b9855cd898cb5503ac4f1d7f
HDVinnie <unit3d@protonmail.com>
2018-09-19 17:31:46 -0400

    (Fix) Review Migration

--- database/migrations/2018_09_16_184221_reviews_table.php
+++ database/migrations/2018_09_16_184221_reviews_table.php
@@ -22,7 +22,7 @@ class ReviewsTable extends Migration
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('episode_id')->references('id')->on('episodes')->onDelete('cascade');
+            $table->foreign('video_id')->references('id')->on('videos')->onDelete('cascade');
         });
     }
 
